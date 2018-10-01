88e8ac7cd003557d701bb7e87d2b8270cab44420
SEMOSS <semoss@semoss.org>
2018-10-01 16:02:43 -0400

    Update for RJava

--- start.R
+++ start.R
@@ -2,6 +2,17 @@
  
 #Include Rserve library
 library(Rserve)
+# Find if port has been set
+port <- Sys.getenv("rserve_port");
+if(port == ""){port = 6311;}
 #Start Rserve
-run.Rserve(debug = TRUE, 6311, remote="enable") 
+run.Rserve(debug = TRUE, port, remote="enable") 
 #Port can also be specified in the configuration file.
+library(rJava)
+.jinit(); 
+.jaddClassPath("/opt/semosshome/dockR/lib");
+.jaddClassPath("/opt/semosshome/dockR/lib/zookeeper-3.4.5.jar");
+.jaddClassPath("/opt/semosshome/dockR/lib/slf4j-api-1.6.1.jar");
+.jaddClassPath("/opt/semosshome/dockR/lib/slf4j-log4j12-1.6.1");
+rj <- .jnew("prerna/cluster/util/RJavaClient", check=TRUE)
+rj$registerZK();
